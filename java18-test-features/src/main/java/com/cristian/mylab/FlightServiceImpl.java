044815b42b0d9ba0f5052e4b784bd2746f60c8a1
cristianprofile <cristianprofile@gmail.com>
2014-08-14 13:33:04 +0200

    created a predicate to lambda boolean expression

--- java18-test-features/src/main/java/com/cristian/mylab/FlightServiceImpl.java
+++ java18-test-features/src/main/java/com/cristian/mylab/FlightServiceImpl.java
@@ -2,25 +2,33 @@ package com.cristian.mylab;
 
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class FlightServiceImpl implements FlightService {
 
+	Predicate<Flight> fullFlightPredicate()
+
+	{
+		return vuelo -> vuelo.getNumPassengers() == vuelo.getNumSeats();
+	}
+
 	@Override
-	public long numberFlightByDay(LocalDate date,Collection<Flight> flightCollection) {
-		// Testing filter method, it used to filter an stream
-		Stream<Flight> filter = flightCollection.stream().filter(vuelo->vuelo.getDate().compareTo(date)==0);
+	public long numberFlightByDay(LocalDate date,
+			Collection<Flight> flightCollection) {
+		// Testing filter method 1, it used to filter an stream
+		Stream<Flight> filter = flightCollection.stream().filter(
+				vuelo -> vuelo.getDate().compareTo(date) == 0);
 		return filter.count();
 	}
-	
+
 	@Override
 	public long numberFullFlight(Collection<Flight> flightCollection) {
-		// Testing filter method, it used to filter an stream
-		Stream<Flight> filter = flightCollection.stream().filter(vuelo->vuelo.getNumPassengers()==vuelo.getNumSeats());
+		// Testing filter method 2, it used to filter an stream
+		Stream<Flight> filter = flightCollection.stream().filter(fullFlightPredicate());
 		return filter.count();
 	}
 
