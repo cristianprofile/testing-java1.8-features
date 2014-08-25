fb29cd79cb62cab78bb83d4f2a600919578459f3
cristianprofile <cristianprofile@gmail.com>
2014-08-25 11:26:42 +0200

    added new test to localdatetime class

--- java18-test-features/src/test/java/com/cristian/mylab/NewFeaturesTest.java
+++ java18-test-features/src/test/java/com/cristian/mylab/NewFeaturesTest.java
@@ -3,10 +3,12 @@ package com.cristian.mylab;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import java.util.Optional;
@@ -23,17 +25,17 @@ public class NewFeaturesTest {
 
 	@Test(expected = IllegalStateException.class)
 	public void testOptionalNewJavaClass() {
-		//create an optional object
+		// create an optional object
 		Optional<String> optional = Optional.of("bam");
 		Assert.assertTrue(optional.isPresent());
-		
-		//setting empty value to optional object
+
+		// setting empty value to optional object
 		optional = Optional.empty();
 		Assert.assertFalse(optional.isPresent());
-		//this line do not write to console output
+		// this line do not write to console output
 		optional.ifPresent(x -> System.out.println(x));
-		
-		//testing orelse method to set a value if optional object is empty
+
+		// testing orelse method to set a value if optional object is empty
 		String orElse = optional.orElse("pepe");
 		Assert.assertTrue(orElse.equals("pepe"));
 		optional.orElseThrow(IllegalStateException::new);
@@ -47,16 +49,17 @@ public class NewFeaturesTest {
 
 		// calculate difference between 2 localtime
 		long hoursBetween = ChronoUnit.HOURS.between(localTime1, localTime2);
-		long minutesBetween = ChronoUnit.MINUTES.between(localTime1, localTime2);
+		long minutesBetween = ChronoUnit.MINUTES
+				.between(localTime1, localTime2);
 		// access localtime methods like to compare if a localTime is before or
 		// after, and hours minutes of difference between 2 dates
 		Assert.assertTrue(localTime1.isBefore(localTime2));
 		Assert.assertFalse(localTime1.isAfter(localTime2));
 		Assert.assertTrue(hoursBetween == 11);
 		Assert.assertTrue(minutesBetween == 660);
-  
-		//deduct hours and minutes of a localtime
-		
+
+		// deduct hours and minutes of a localtime
+
 		Duration ofHours = Duration.ofHours(1);
 		LocalTime newTime = localTime1.minus(ofHours);
 		Assert.assertTrue(newTime.getHour() == 11);
@@ -70,16 +73,13 @@ public class NewFeaturesTest {
 		LocalTime minus = newTimeMinusSecond.minus(minusMinutes);
 		Assert.assertTrue(minus.getMinute() == 50);
 
-		//create a datetime with uk formatter
-		
-		DateTimeFormatter ukFormatter =
-			    DateTimeFormatter
-			        .ofLocalizedTime(FormatStyle.SHORT)
-			        .withLocale(Locale.UK);
+		// create a datetime with uk formatter
+
+		DateTimeFormatter ukFormatter = DateTimeFormatter.ofLocalizedTime(
+				FormatStyle.SHORT).withLocale(Locale.UK);
 
 		LocalTime leetTime = LocalTime.parse("13:37", ukFormatter);
-		
-		
+
 		Assert.assertTrue(leetTime.getHour() == 13);
 		Assert.assertTrue(leetTime.getMinute() == 37);
 		Assert.assertTrue(leetTime.getSecond() == 0);
@@ -93,17 +93,57 @@ public class NewFeaturesTest {
 		// created a localdate 2018-12-11 (yyyy mm dd)
 
 		LocalDate localDate = LocalDate.of(2018, 12, 11);
-		
-		//Access day of week day of month day of year and month
-		
+
+		// Access day of week day of month day of year and month
+
 		DayOfWeek dayOfWeek = localDate.getDayOfWeek();
 		int dayOfMonth = localDate.getDayOfMonth();
 		Month month = localDate.getMonth();
-		
+
 		Assert.assertTrue(dayOfWeek.equals(DayOfWeek.TUESDAY));
 		Assert.assertTrue(dayOfMonth == 11);
 		Assert.assertTrue(localDate.getDayOfYear() == 345);
 		Assert.assertTrue(month.equals(Month.DECEMBER));
+
+		DateTimeFormatter germanFormatter = DateTimeFormatter.ofLocalizedDate(
+				FormatStyle.MEDIUM).withLocale(Locale.GERMAN);
+
+		LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);
+
+		Assert.assertTrue(xmas.getMonth().equals(Month.DECEMBER));
+		Assert.assertTrue(xmas.getYear() == 2014);
+
+	}
+
+	@Test
+	public void testNewDateApiLocalDateTime() {
+
+		LocalDateTime localDateTime = LocalDateTime.of(2014, Month.DECEMBER,
+				31, 23, 59, 59);
+
+		DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
+		Assert.assertTrue(dayOfWeek.equals(DayOfWeek.WEDNESDAY));
+
+		Month month = localDateTime.getMonth();
+		Assert.assertTrue(month.equals(Month.DECEMBER));
+
+		long minuteOfDay = localDateTime.getLong(ChronoField.MINUTE_OF_DAY);
+		Assert.assertTrue(minuteOfDay == 1439);
+
+		long dayOfYear = localDateTime.getLong(ChronoField.DAY_OF_YEAR);
+		Assert.assertTrue(dayOfYear == 365);
+
+		DateTimeFormatter formatter = DateTimeFormatter
+				.ofPattern("MMM dd, yyyy - HH:mm");
+
+		LocalDateTime parseLocalDateTime = LocalDateTime.parse(
+				"Nov 03, 2014 - 07:13", formatter);
+
+		Assert.assertTrue(parseLocalDateTime.getMonth().equals(Month.NOVEMBER));
+		Assert.assertTrue(parseLocalDateTime.getYear() == 2014);
+		Assert.assertTrue(parseLocalDateTime.getHour() == 7);
+		Assert.assertTrue(parseLocalDateTime.getMinute() == 13);
+		Assert.assertTrue(parseLocalDateTime.getSecond() == 0);
 
 	}
 
